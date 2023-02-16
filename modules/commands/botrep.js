5976db956b0002f2ade56a6b67093320455edd9f
tuantvtvip <trantientuan37@gmail.com>
2023-02-16 21:45:28 +0700

    up

--- modules/commands/botrep.js
+++ modules/commands/botrep.js
@@ -16,8 +16,10 @@ module.exports.config = {
         var { threadID, messageID, senderID } = event;
         var tag = (await Users.getData(senderID)).name;
         let output = "ad của bot đz ";
-        let varinput = [ "tuan","tuantvt","tuanvip","tuandz" ];
+        let varinput = [ "tuan","tuantvt","tuanvip","tuandz","tuann","tuân" ];
         let varinput1 = [ "adminbot", "adbot", "facebook" ,"fb" ,"in4" ,"info"];
+        let varinput2 = [ "@Overnine Houtarou"]
+        let varinput3 = [ "bot cute","bot vip","yêu bot","yeubot"]
         for (const input of varinput) {
             if (event.body.indexOf(input)==0 && event.body.length == input.length) {
             var job = ['Vui lòng nhắn riêng với ad của bot https://www.facebook.com/tuantvtvip',
@@ -32,7 +34,19 @@ module.exports.config = {
             var job = ['ad của bot đây ạ: https://www.facebook.com/tuantvtvip 😘'];
                  api.sendMessage(job[Math.floor(Math.random() * job.length)] + ' '  , threadID, messageID);
             }     
-        }        
+        }    
+        for (const input of varinput2) {
+            if (event.body.indexOf(input)==0 && event.body.length == input.length) {
+            var job = ['tag gì ad bot yêu sao ;)','dùng lệnh callad và bắn tin riêng bạn sẽ được ad tui yêu thương'];
+                 api.sendMessage(job[Math.floor(Math.random() * job.length)] + ' '  , threadID, messageID);
+            }     
+        }
+        for (const input of varinput3) {
+            if (event.body.indexOf(input)==0 && event.body.length == input.length) {
+            var job = ['bot yêy bạn lắm ạ 💗'];
+                 api.sendMessage(job[Math.floor(Math.random() * job.length)] + ' '  , threadID, messageID);
+            }     
+        }     
         }
         module.exports.run = function({}) {
     }
